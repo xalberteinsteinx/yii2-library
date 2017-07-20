8e07a1f92459f2cb3e4c86291ce710d27bbedd28
Albert <xalbert.einsteinx@gmail.com>
2017-07-21 02:38:12 +0300

    Add LanguageSwitcher widget for backend.

---
+++ backend/widgets/LanguageSwitcher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace xalberteinsteinx\library\backend\widgets;
+use bl\multilang\entities\Language;
+use yii\base\Widget;
+
+/**
+ * This widget adds language switcher button.
+ *
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class LanguageSwitcher extends Widget
+{
+    public $languages;
+    public $selectedLanguage;
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        if (empty($this->languages)) {
+            $this->languages = Language::find()->where(['active' => true])->all();
+        }
+        return $this->render('language-switcher', [
+            'languages' => $this->languages,
+            'selectedLanguage' => $this->selectedLanguage,
+        ]);
+    }
+}
---
+++ backend/widgets/views/language-switcher.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ *
+ * @var $selectedLanguage \bl\multilang\entities\Language
+ * @var $languages \bl\multilang\entities\Language[]
+ * @var $model \yii\db\ActiveRecord
+ */
+
+use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Url;
+?>
+
+<?php if (count($languages) > 1): ?>
+    <div class="dropdown pull-right">
+        <button class="btn btn-warning btn-xs dropdown-toggle" data-toggle="dropdown">
+            <span>
+                <?= $selectedLanguage->name . ' ' . FA::i(FA::_ANGLE_DOWN) ?>
+            </span>
+        </button>
+        <?php if (count($languages) > 1): ?>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <?php foreach ($languages as $language): ?>
+                    <li>
+
+                        <?php $module = (Yii::$app->controller->module->id != Yii::$app->id) ? '/' . Yii::$app->controller->module->id : ''; ?>
+
+                        <a href="<?= Url::to([$module . '/' . Yii::$app->controller->id . '/' . Yii::$app->controller->action->id,
+                            'id' => Yii::$app->request->get('id'),
+                            'languageId' => $language->id]); ?>">
+                            <?= $language->name ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
