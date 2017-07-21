fc32e60514fd03ce60e2d5e590f96cce73086e69
Albert <xalbert.einsteinx@gmail.com>
2017-07-21 13:26:29 +0300

    Add TranslationBehavior for Article and ArticleCategory entities.

--- common/entities/Article.php
+++ common/entities/Article.php
@@ -5,7 +5,9 @@ namespace xalberteinsteinx\library\common\entities;
 use bl\multilang\behaviors\TranslationBehavior;
 use dektrium\user\models\User;
 use Yii;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 use yii2tech\ar\position\PositionBehavior;
 
 /**
@@ -50,6 +52,14 @@ class Article extends ActiveRecord
     public function behaviors()
     {
         return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
             'translation' => [
                 'class' => TranslationBehavior::className(),
                 'translationClass' => ArticleTranslation::className(),
--- common/entities/ArticleCategory.php
+++ common/entities/ArticleCategory.php
@@ -5,7 +5,9 @@ namespace xalberteinsteinx\library\common\entities;
 use bl\multilang\behaviors\TranslationBehavior;
 use dektrium\user\models\User;
 use Yii;
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 use yii2tech\ar\position\PositionBehavior;
 
 /**
@@ -47,6 +49,14 @@ class ArticleCategory extends ActiveRecord
     public function behaviors()
     {
         return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
             'translation' => [
                 'class' => TranslationBehavior::className(),
                 'translationClass' => ArticleCategoryTranslation::className(),
