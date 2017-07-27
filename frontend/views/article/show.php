f923617c06c23fa424a91665586c40ba335dd71c
Albert <xalbert.einsteinx@gmail.com>
2017-07-27 19:23:53 +0300

    Change article and category show view.

--- frontend/views/article/show.php
+++ frontend/views/article/show.php
@@ -5,6 +5,7 @@
  * @var $article       \xalberteinsteinx\library\common\entities\Article
  */
 
+use xalberteinsteinx\library\frontend\widgets\ImageSlider;
 use yii\helpers\Url;
 
 $this->title = $article->translation->title;
@@ -38,5 +39,110 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?= $article->translation->intro_text; ?>
         <?= $article->translation->full_text; ?>
+
+
+        <?php if (count($article->images) > 1) : ?>
+
+            <section class="slider">
+                <!-- Images slides -->
+                <div class="slides">
+                    <?= ImageSlider::widget([
+                        'article' => $article,
+                        'fancyBox' => true,
+                        'defaultImage' => \Yii::$app->params['defaultImage'],
+                        'fancyBoxWidgetConfig' => [
+                            'helpers' => true,
+                            'mouse' => true,
+                            'config' => [
+                                'maxWidth' => '100%',
+                                'maxHeight' => '100%',
+                                'playSpeed' => 7000,
+                                'padding' => 0,
+                                'fitToView' => true,
+                                'width' => '90%',
+                                'height' => '90%',
+                                'autoSize' => false,
+                                'closeClick' => false,
+                                'openEffect' => 'elastic',
+                                'closeEffect' => 'elastic',
+                                'prevEffect' => 'elastic',
+                                'nextEffect' => 'elastic',
+                                'openOpacity' => true,
+                                'helpers' => [
+                                    'title' => ['type' => 'float'],
+                                    'buttons' => [],
+                                    'thumbs' => ['width' => 70, 'height' => 70],
+                                    'overlay' => [
+                                        'css' => [
+                                            'background' => 'rgba(0, 0, 0, 0.8)'
+                                        ]
+                                    ]
+                                ],
+                            ]
+                        ],
+                        'containerOptions' => [
+                            'id' => 'articleImageSlider',
+                        ],
+                        /** @see http://kenwheeler.github.io/slick/#settings */
+                        'clientOptions' => [
+                            'autoplay' => true,
+                            //'centerMode' => true,
+                            'dots' => true,
+                            'asNavFor' => '#articleImageSliderThumbs'
+                        ],
+                    ]); ?>
+                </div>
+
+                <!-- Slide thumbs -->
+                <div class="thumbs">
+                    <?= ImageSlider::widget([
+                        'article' => $article,
+                        'imagesSize' => 'hd',
+                        'defaultImage' => \Yii::$app->params['defaultImage'],
+                        'containerOptions' => [
+                            'id' => 'articleImageSliderThumbs',
+                            'class' => 'article-image-thumbs',
+                        ],
+                        'itemOptions' => [
+                            'class' => 'article-image-slide-thumb'
+                        ],
+                        /** @see http://kenwheeler.github.io/slick/#settings */
+                        'clientOptions' => [
+                            'autoplay' => true,
+                            'asNavFor' => '#articleImageSlider',
+                            'slidesToShow' => 7,
+                            'slidesToScroll' => 1,
+                            'dots' => false,
+                            'focusOnSelect' => true,
+                            'vertical' => false,
+                            'responsive' => [
+                                [
+                                    'breakpoint' => 992,
+                                    'settings' => [
+                                        'slidesToShow' => 6,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                                [
+                                    'breakpoint' => 480,
+                                    'settings' => [
+                                        'slidesToShow' => 4,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                                [
+                                    'breakpoint' => 360,
+                                    'settings' => [
+                                        'slidesToShow' => 3,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                            ]
+                        ],
+                    ]); ?>
+                </div>
+            </section>
+        <?php endif; ?>
+
     </div>
 </article>
--- frontend/views/category/show.php
+++ frontend/views/category/show.php
@@ -6,6 +6,7 @@
  */
 
 use bl\imagable\helpers\FileHelper;
+use xalberteinsteinx\library\frontend\widgets\ImageSlider;
 use yii\helpers\Url;
 
 $this->title = $category->translation->title;
@@ -13,7 +14,23 @@ $this->title = $category->translation->title;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<article>
+<article class="flex">
+    <?php if (!empty($parent = $category->parent)): ?>
+    <aside>
+
+            <p class="h2">
+                <?= $parent->translation->title; ?>
+            </p>
+            <?php foreach ($parent->childCategories as $childCategory) : ?>
+                <p>
+                    <a href="<?= Url::to(['/library/article/show', 'id' => $childCategory->id]); ?>">
+                        <?= $childCategory->translation->title; ?>
+                    </a>
+                </p>
+            <?php endforeach; ?>
+    </aside>
+    <?php endif; ?>
+
     <div class="article-content">
         <h1 class="text-center">
             <?= $this->title; ?>
@@ -25,6 +42,33 @@ $this->params['breadcrumbs'][] = $this->title;
         <div>
             <?= $category->translation->full_text; ?>
         </div>
+
+        <?php if ($category->hasChildCategories()) : ?>
+            <!--Child categories-->
+            <section class="category-articles">
+                <div class="articles-grid blue">
+                    <?php foreach ($category->childCategories as $childCategory) : ?>
+                        <?php $link = Url::to(['/library/category/show', 'id' => $childCategory->id]); ?>
+                        <section class="article">
+                            <a href="<?= $link; ?>">
+                                <?php $imageName = $childCategory->images[0]->image_name; ?>
+                                <?php if (!empty($imageName)): ?>
+                                    <img align="left" src="/images/library/category/<?= FileHelper::getFullName(
+                                        \Yii::$app->library_imagable->get('category', 'thumb', $imageName)
+                                    ); ?>" alt="<?= $staticPage->translation->title; ?>">
+                                <?php endif; ?>
+
+                                <h2>
+                                    <?= $childCategory->translation->title; ?>
+                                </h2>
+                            </a>
+
+                        </section>
+                    <?php endforeach; ?>
+                </div>
+
+            </section>
+        <?php endif; ?>
 
         <?php if ($category->hasArticles()) : ?>
             <!--Articles-->
@@ -50,6 +94,110 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?php endforeach; ?>
                 </div>
 
+            </section>
+        <?php endif; ?>
+
+
+        <?php if (count($category->images) > 1) : ?>
+
+            <section class="slider">
+                <!-- Images slides -->
+                <div class="slides">
+                    <?= ImageSlider::widget([
+                        'article' => $category,
+                        'fancyBox' => true,
+                        'defaultImage' => \Yii::$app->params['defaultImage'],
+                        'fancyBoxWidgetConfig' => [
+                            'helpers' => true,
+                            'mouse' => true,
+                            'config' => [
+                                'maxWidth' => '100%',
+                                'maxHeight' => '100%',
+                                'playSpeed' => 7000,
+                                'padding' => 0,
+                                'fitToView' => true,
+                                'width' => '90%',
+                                'height' => '90%',
+                                'autoSize' => false,
+                                'closeClick' => false,
+                                'openEffect' => 'elastic',
+                                'closeEffect' => 'elastic',
+                                'prevEffect' => 'elastic',
+                                'nextEffect' => 'elastic',
+                                'openOpacity' => true,
+                                'helpers' => [
+                                    'title' => ['type' => 'float'],
+                                    'buttons' => [],
+                                    'thumbs' => ['width' => 70, 'height' => 70],
+                                    'overlay' => [
+                                        'css' => [
+                                            'background' => 'rgba(0, 0, 0, 0.8)'
+                                        ]
+                                    ]
+                                ],
+                            ]
+                        ],
+                        'containerOptions' => [
+                            'id' => 'articleImageSlider',
+                        ],
+                        /** @see http://kenwheeler.github.io/slick/#settings */
+                        'clientOptions' => [
+                            'autoplay' => true,
+                            //'centerMode' => true,
+                            'dots' => true,
+                            'asNavFor' => '#articleImageSliderThumbs'
+                        ],
+                    ]); ?>
+                </div>
+
+                <!-- Slide thumbs -->
+                <div class="thumbs">
+                    <?= ImageSlider::widget([
+                        'article' => $category,
+                        'imagesSize' => 'hd',
+                        'defaultImage' => \Yii::$app->params['defaultImage'],
+                        'containerOptions' => [
+                            'id' => 'articleImageSliderThumbs',
+                            'class' => 'article-image-thumbs',
+                        ],
+                        'itemOptions' => [
+                            'class' => 'article-image-slide-thumb'
+                        ],
+                        /** @see http://kenwheeler.github.io/slick/#settings */
+                        'clientOptions' => [
+                            'autoplay' => true,
+                            'asNavFor' => '#articleImageSlider',
+                            'slidesToShow' => 7,
+                            'slidesToScroll' => 1,
+                            'dots' => false,
+                            'focusOnSelect' => true,
+                            'vertical' => false,
+                            'responsive' => [
+                                [
+                                    'breakpoint' => 992,
+                                    'settings' => [
+                                        'slidesToShow' => 6,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                                [
+                                    'breakpoint' => 480,
+                                    'settings' => [
+                                        'slidesToShow' => 4,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                                [
+                                    'breakpoint' => 360,
+                                    'settings' => [
+                                        'slidesToShow' => 3,
+                                        'vertical' => false,
+                                    ]
+                                ],
+                            ]
+                        ],
+                    ]); ?>
+                </div>
             </section>
         <?php endif; ?>
 
